9bcc51398f7a4c0f87c1d1251e8d16804d578124
Nwabiukwu Christian <christianugoo@gmail.com>
2022-07-08 02:24:12 -0700

    correction

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,9 +8,7 @@
 int main(void)
 {
 	int i;
-
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
 
 	/**
@@ -21,6 +19,5 @@ int main(void)
 	 */
 
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
