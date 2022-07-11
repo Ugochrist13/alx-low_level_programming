8b5e597f396a6972d73585fc99eb34184459bf98
Nwabiukwu Christian <christianugoo@gmail.com>
2022-07-11 12:22:51 -0700

    corrections

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,8 +14,8 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 400 == 0 || year % 4 == 0) && !(year % 100 == 0))
 	{
-		if (month >= 3 && day >= 60)
-		{
+		if (month > 2 && day >= 60)
+i		{
 			day++;
 		}
 		printf("Day of the year: %d\n", day);
